b0efcd39720e7800866bba6c3fcf4741f8bfa480
Vido <vidoseaver@gmail.com>
2016-08-24 17:37:17 -0600

    node refactored and tests added

--- test/node_test.rb
+++ test/node_test.rb
@@ -5,55 +5,81 @@ require_relative "../lib/node"
 class NodeTest < Minitest::Test
 
   def setup
-    @node1 = Node.new(50, "a medoicre movie")
-    @node2 = Node.new(75, "a decent movie")
-    @node3 = Node.new(25, "a bad movie")
-    @node4 = Node.new(5, "a awful movie")
-    @node5 = Node.new(1, "the worst movie")
-
+    @node_1 = Node.new(50, "a medoicre movie")
+    @node_2 = Node.new(75, "a decent movie")
+    @node_3 = Node.new(25, "a bad movie")
+    @node_4 = Node.new(5, "a awful movie")
+    @node_5 = Node.new(1, "the worst movie")
   end
 
   def test_it_exists
-    assert_instance_of Node, @node1
+    assert_instance_of Node, @node_1
   end
 
   def test_the_node_has_a_rating
-    assert 50, @node1.rating
+    assert 50, @node_1.rating
+  end
+
+  def test_left_child_empty_returns_correctly
+   @node_1.node = @node_3
+   assert @node_1.left_child_empty?
+   @node_1.decider(@node_3)
+   refute @node_1.left_child_empty?
+  end
+
+  def test_right_child_empty_returns_correctly
+   @node_1.node = @node_2
+   assert @node_1.right_child_empty?
+   @node_1.decider(@node_2)
+   refute @node_1.right_child_empty?
+  end
+
+  def test_left_child_filled_returns_correctly
+    @node_1.node = @node_3
+    refute @node_1.left_child_filled?
+    @node_1.decider(@node_3)
+    assert @node_1.left_child_filled?
+  end
+
+  def test_right_child_filled_returns_correctly
+    @node_1.node = @node_2
+    refute @node_1.right_child_filled?
+    @node_1.decider(@node_2)
+    assert @node_1.right_child_filled?
   end
 
   def test_the_node_has_a_name
-    assert 50, @node1.name
+    assert 50, @node_1.name
   end
 
   def test_it_knows_if_it_has_children
-    refute @node1.left_child?
-    refute @node1.right_child?
+    refute @node_1.left_child?
+    refute @node_1.right_child?
   end
+
 
   def test_it_can_store_and_pass_nodes
-    assert_equal nil, @node1.left_child
-    assert_equal nil, @node1.right_child
-    @node1.decider(@node3)
-    assert_instance_of Node, @node1.left_child
-    @node1.decider(@node2)
-    assert_instance_of Node, @node1.right_child
+    assert_equal nil, @node_1.left_child
+    assert_equal nil, @node_1.right_child
+    @node_1.decider(@node_3)
+    assert_instance_of Node, @node_1.left_child
+    @node_1.decider(@node_2)
+    assert_instance_of Node, @node_1.right_child
   end
 
-  def test_it_returns_the_childs_rating_if_the_spot_is_filled
-    @node1.decider(@node3)
-    assert_instance_of Node, @node1.decider(@node4)
+  def test_it_returns_the_childs_depth_if_the_spot_gets_filled
+    @node_1.decider(@node_3)
+    assert_equal 1, @node_1.decider(@node_4).depth
   end
 
-  def test_printing_changes_printed_to_true
 
-  end
 
-  def test_reset_print
-    refute @node1.printed?
-    assert @node1.print
-    assert @node1.printed?
-    @node1.reset_print
-    refute @node1.printed?
+  def test_reset_print_sets_print_false
+    refute @node_1.printed?
+    assert @node_1.print
+    assert @node_1.printed?
+    @node_1.reset_print
+    refute @node_1.printed?
   end
 
 
